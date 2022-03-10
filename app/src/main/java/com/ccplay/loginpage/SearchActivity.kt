947d56c8af8c184df97a5f8e2f161285c9935000
cc <cc_xie@gmail.com>
2022-03-10 16:11:03 +0800

    Initial commit

---
+++ app/src/main/java/com/ccplay/loginpage/SearchActivity.kt
@@ -0,0 +1,11 @@
+package com.ccplay.loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
