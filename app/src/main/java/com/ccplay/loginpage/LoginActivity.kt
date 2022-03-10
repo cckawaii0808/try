290d3128e2479640640d444a3dbd3e59797cdb44
cc <cc_xie@gmail.com>
2022-03-10 19:03:41 +0800

    進入聊天室並歡迎

--- app/src/main/java/com/ccplay/loginpage/LoginActivity.kt
+++ app/src/main/java/com/ccplay/loginpage/LoginActivity.kt
@@ -1,15 +1,19 @@
 package com.ccplay.loginpage
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.ccplay.loginpage.databinding.ActivityLoginBinding
 
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val TAG = LoginActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -23,18 +27,13 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(intent)
             } else {
                 Log.d(TAG, "密碼錯誤")
+                 AlertDialog.Builder(this)
+                    .setTitle("登入失敗")
+                    .setMessage("請重新嘗試")
+                    .setPositiveButton("ok", null)
+                    .show()
             }
 
-
-//            val username = binding.editTextTextPersonName.text.toString()
-//            val password = binding.editTextTextPassword.text.toString()
-//            if (username == "cc" &&
-//                password == "1234"
-//            ) {
-
-
-
-
-        }//
+        }
     }
 }
--- app/src/main/java/com/ccplay/loginpage/RoomActivity.kt
+++ app/src/main/java/com/ccplay/loginpage/RoomActivity.kt
@@ -2,10 +2,21 @@ package com.ccplay.loginpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.ccplay.loginpage.databinding.ActivityLoginBinding
+import com.ccplay.loginpage.databinding.ActivityRoomBinding
 
 class RoomActivity : AppCompatActivity() {
+    private var TAG = RoomActivity::class.java.simpleName
+    lateinit var binding: ActivityRoomBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_room)
+        binding = ActivityRoomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG, "歡迎光臨聊天室")
+        Toast.makeText(this, "歡迎光臨聊天室，請注意發言".toString(), Toast.LENGTH_LONG).show()//顯示bmi在小訊息匡
     }
+
+
 }
