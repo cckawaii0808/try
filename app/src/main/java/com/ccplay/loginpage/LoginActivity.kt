6c6f234d902c5d6aab916fe32f1abf144adb046c
cc <cc_xie@gmail.com>
2022-03-11 15:32:45 +0800

    增加筆記

--- app/src/main/java/com/ccplay/loginpage/LoginActivity.kt
+++ app/src/main/java/com/ccplay/loginpage/LoginActivity.kt
@@ -15,37 +15,42 @@ import com.ccplay.loginpage.databinding.ActivityRoomBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-    var remember = false
+
     private val TAG = LoginActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val pref = getSharedPreferences("USERDATA", Context.MODE_PRIVATE)
-        val prefUser = pref.getString("USER", "")
-        val checked = pref.getBoolean("rem_yes", false)
-        binding.ckremember.isChecked = checked
-        binding.ckremember.setOnCheckedChangeListener { compoundButton, checked ->
-            remember = checked
-            pref.edit().putBoolean("rem_yes", remember).apply()
+        loginandremember()
+    }
+
+    private fun loginandremember() {
+        var remember = false//宣告一個為否的變數存不打勾
+        val pref = getSharedPreferences("USERDATA", MODE_PRIVATE)//設定資料夾名字
+        val prefUser = pref.getString("USER", "")//設定預設本地存帳號字串
+        val checked = pref.getBoolean("rem_yes", false)//設定本地存預設勾選是否
+        binding.ckremember.isChecked = checked//記憶我的勾選值為取得的布林值
+        binding.ckremember.setOnCheckedChangeListener { compoundButton, checked ->//設定監聽如果勾選
+            remember = checked//現在的勾選狀態同步到本地
+            pref.edit().putBoolean("rem_yes", remember).apply()//本地存狀態
             if (!checked) {
                 pref.edit().putString("USER", "").apply()
             }
         }
-        if (prefUser != "") {
+        if (prefUser != "") {//如果本地有值，請幫我輸入使用者帳號
             binding.etuser.setText(prefUser)
         }
-        binding.blogin.setOnClickListener {
+        binding.blogin.setOnClickListener {//當我按下登入
             val username = binding.etuser.text.toString()
             val password = binding.etpass.text.toString()
             if (username == "cc" && password == "1234") {
 
-                if (remember) {
-                    pref.edit().putString("USER", username).putInt("LEVEL", 3).apply()
+                if (remember) {//如果勾選記憶我，放我的名字到本地的使用者帳號
+                    pref.edit().putString("USER", username).putInt("LEVEL", 3).apply()//
                     Log.d(TAG, "密碼正確")
                 }
-                val intent = Intent(this, RoomActivity::class.java)
-                startActivity(intent)
+                val intent = Intent(this, RoomActivity::class.java)//設定跳轉位置
+                startActivity(intent)//執行跳轉
             } else {
                 Log.d(TAG, "密碼錯誤")
                 AlertDialog.Builder(this)
